d491bf509812f451867c0581c764280fa1596034
hyuk-dev <hyuk.development@gmail.com>
2025-05-07 11:25:59 +0900

    feat: google-site-verification

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -57,7 +57,6 @@ export default function Home() {
         type="button"
         onClick={() => { setUrl((prev) => "")}}
         variant="contained"
-        loading={loading}
         sx={{ bgcolor: "red "}}
       >
         reset
